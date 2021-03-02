09aeeb2bbe4361aaa0061a9f5c9196bdb5a194b9
timvvoodman <71102666+timvvoodman@users.noreply.github.com>
2021-03-02 15:30:18 -0500

    aditional file set up and dependency instalation

---
+++ models/book.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  author: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  link: { type: String, required: true },
+  title: { type: String, required: true },
+
+  date: { type: Date, default: Date.now },
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
